ddd1ad0b555b6e1f3f81e5a8628999d13a008ff8
Ali Sabil <ali.sabil@gmail.com>
2007-01-07 14:08:55 +0530

    - Added the import helpers

---
+++ pymsn/gnet/util/OpenSSL.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2005  Ole André Vadla Ravnås <oleavr@gmail.com>
+# Copyright (C) 2006  Ali Sabil <ali.sabil@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+"""OpenSSL independent from the available distribution"""
+
+from M2Crypto.m2 import *
+import M2Crypto.SSL.cb as Callback
+from M2Crypto.SSL import SSLError
+
+SSL_ERROR_NONE = ssl_error_none
+SSL_ERROR_ZERO_RETURN = ssl_error_zero_return
+SSL_ERROR_WANT_READ = ssl_error_want_read
+SSL_ERROR_WANT_WRITE = ssl_error_want_write
+SSL_ERROR_WANT_X509_LOOKUP = ssl_error_want_x509_lookup
+SSL_ERROR_SSL = ssl_error_ssl
