095288416404bd3a41f8858dbc587b74fb915be5
Ali Sabil <ali.sabil@gmail.com>
2007-07-19 05:54:02 +0100

    - Merged jprieur's branch

---
+++ pymsn/service2/AddressBook/scenario/accept_invite.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2007 Johann Prieur <johann.prieur@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+
+class AcceptInviteScenario(BaseScenario):
+    def __init__(self, sharing, callback, errback):
+        """Accepts an invitation.
+
+            @param sharing: the membership service
+            @param callback: tuple(callable, *args)
+            @param errback: tuple(callable, *args)
+        """
+        BaseScenario.__init__(self, 'ContactMsgrAPI', callback, errback)
+        self.__sharing = sharing
+
+    def execute(self):
+        # Add member & contact list
+        # add using ContactAdd
+        # remove from Pending using DeleteMember
+        # add to using Allow using AddMember
+
+        # Add member only
+        # remove from Pending using DeleteMember
+        # add to using Allow using AddMember        
+        pass
