2f1800e3b42b412a1a49a52fbb2f9603c0b737ab
Ali Sabil <ali.sabil@gmail.com>
2007-08-06 22:25:44 +0200

    - Improved the msnp2p BaseP2PTransport

--- pymsn/msnp2p/transport.py
+++ pymsn/msnp2p/transport.py
@@ -36,6 +36,11 @@ def _generate_id():
     """
     return random.randint(1000, sys.maxint)
 
+_previous_chunk_id = random.randint(1000, 2 ** 31 - 1)
+def _chunk_id():
+    _previous_chunk_id += 1
+    return _previous_chunk_id
+
 class TLPHeader(object):
     SIZE = 48
 
@@ -100,6 +105,22 @@ class MessageChunk(object):
     def __str__(self):
         return str(self.header) + str(self.body)
 
+    def is_control_chunk(self):
+        return self.header.flags & 0xFFFFFFCF
+
+    def is_ack_chunk(self):
+        return self.header.flags & (TLPFlag.NAK | TLPFlag.ACK)
+
+    def require_ack(self):
+        if self.is_ack_chunk():
+            return False
+        if self.header.flags & TLPFlag.EACH:
+            return True
+        current_size = self.header.chunk_size + self.header.blob_offset
+        if current_size >= self.header.blob_size:
+            return True
+        return False
+
     @staticmethod
     def parse(data):
         header = TLPHeader.parse(data[:48])
@@ -160,6 +181,7 @@ class MessageBlob(object):
         header.blob_offset = blob_offset
         header.blob_size = self.total_size
         header.chunk_size = len(data)
+        header.dw1 = _chunk_id()
 
         return MessageChunk(header, data)
 
@@ -175,7 +197,7 @@ class ControlBlob(MessageBlob):
         MessageBlob.__init__(self, 0, None)
         header = TLPHeader(session_id, self.blob_id, 0, 0, 0,
                 flags, dw1, dw2, qw1)
-        self.chunk = MessageChunk(header, "")
+        self.chunk = MessageChunk(header, None)
 
     def get_chunk(self):
         return self.chunk
@@ -199,11 +221,7 @@ class BaseP2PTransport(gobject.GObject):
         gobject.GObject.__init__(self)
         self._client = client
         self._name = name
-        self._seq_num = randome.randint(3600, 86400) * 1000
-
-    @property
-    def sequence_num(self):
-        return self._seq_num
+        self._reset()
 
     @property
     def name(self)
@@ -222,27 +240,73 @@ class BaseP2PTransport(gobject.GObject):
         raise NotImplementedError
 
     def send(self, blob, callback=None, errback=None):
+        if blob.is_control_blob():
+            self._control_blob_queue.append(blob)
+        else:
+            self._data_blob_queue.append(blob)
+        self._process_send_queues()
+
+    def _send_chunk(self, chunk):
         raise NotImplementedError
 
     # Helper methods
-    def _is_control_chunk(self, chunk):
-        return chunk.header.flags & 0xFFFFFFCF
+    def _reset(self):
+        self._control_blob_queue = []
+        self._data_blob_queue = []
+        self._pending_ack = {} # blob_id : [blob_offset1, blob_offset2 ...]
 
-    def _increment_sequence_num(self):
-        self._seq_num += 1
+    def _add_pending_ack(self, blob_id, chunk_id=0):
+        if blob_id not in self._pending_ack:
+            self._pending_ack[blob_id] = set()
+        self._pending_ack[blob_id].add(chunk_id)
 
-        if self._seq_num >= 2 ** 32 - 1
-            self._seq_num = 1
+    def _del_pending_ack(self, blob_id, chunk_id=0):
+        if blob_id not in self._pending_ack:
+            return
+        self._pending_ack[blob_id].discard(chunk_id)
 
-    def _try_send_ack(self, received_chunk):
+        if len(self._pending_ack[blob_id]) == 0:
+            del self._pending_ack[blob_id]
+
+    def _on_chunk_received(self, chunk):
+        if chunk.require_ack():
+            self._send_ack(chunk)
+
+        if chunk.header.flags & TLPFlag.ACK:
+            self._del_pending_ack(chunk.header.dw1, chunk.header.dw2)
+
+        #FIXME: handle all the other flags
+
+        if not chunk.is_control_chunk():
+            self.emit("chunk-received", chunk)
+
+        self._process_send_queues()
+
+    def _on_chunk_sent(self, chunk):
+        self.emit("chunk-sent", chunk)
+        self._process_send_queues()
+
+    def _process_send_queues(self):
+        if len(self._control_blob_queue) > 0:
+            queue = self._control_blob_queue
+        elif len(self._data_blob_queue) > 0:
+            queue = self._data_blob_queue
+        else:
+            return
+
+        blob = queue[0]
+        chunk = blob.get_chunk(self.max_chunk_size)
+        if blob.is_complete():
+            queue.pop(0) # FIXME: we should keep it in the queue until we receive the ACK
+
+        if chunk.require_ack() :
+            self._add_pending_ack(chunk.header.blob_id, chunk.header.dw1)
+        self._send_chunk(chunk)
+
+    def _send_ack(self, received_chunk):
         flags = chunk.header.flags
 
-        if flags & (TLPFlag.NAK | TLPFlag.ACK):
-            return
-        
-        current_size = chunk.header.blob_offset + chunk.header.chunk_size
-        if current_size < chunk.header.blob_size and \
-                not (flags & TLPFlag.EACH):
+        if not chunk.require_ack():
             return
         
         flags = TLPFlag.ACK
