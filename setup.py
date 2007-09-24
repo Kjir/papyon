3b792706996225f081654b77a9a8cd6c967f4451
Ali Sabil <ali.sabil@gmail.com>
2007-09-24 23:35:53 +0200

    Fixed the setup.py to include all the packages

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup
-from sys import version
+import os
+
 
 # Metadata
 NAME = "pymsn"
@@ -10,6 +11,41 @@ AUTHOR_EMAIL = "ali.sabil@gmail.com"
 URL = "http://telepathy.freedesktop.org/wiki/Pymsn"
 LICENSE = "GNU GPL"
 
+# Compile the list of packages available, because distutils doesn't have
+# an easy way to do this.
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+pymsn_dir = os.path.join(root_dir, 'pymsn')
+pieces = fullsplit(root_dir)
+if pieces[-1] == '':
+    len_root_dir = len(pieces) - 1
+else:
+    len_root_dir = len(pieces)
+
+for dirpath, dirnames, filenames in os.walk(pymsn_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)[len_root_dir:]))
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
 # Setup
 setup(name=NAME,
       version=VERSION,
@@ -19,7 +55,7 @@ setup(name=NAME,
       url=URL,
       license=LICENSE,
       platforms=["any"],
-      packages=["pymsn"],
+      packages=packages,
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Environment :: Console',
