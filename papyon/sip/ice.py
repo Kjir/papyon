ab0d418910d15d749a51244da7bf1908cafcd316
Louis-Francis Ratté-Boulianne <louis-francis.ratte-boulianne@collabora.co.uk>
2009-05-15 15:07:01 -0400

    Added support for ICE candidates and codecs negociation (+ tests)

--- papyon/sip/ice.py
+++ papyon/sip/ice.py
@@ -18,10 +18,154 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+from papyon.sip.constants import *
+from papyon.sip.sdp import *
 from papyon.util.decorator import rw_property
 
 class Session(object):
-    pass
+
+    def __init__(self, draft=0):
+        self.draft = draft
+        self._local_codecs = {}
+        self._remote_codecs = {}
+        self._local_candidates = {}
+        self._remote_candidates = {}
+        self._local_active = {}
+        self._remote_active = {}
+
+    def get_remote_codecs(self, name):
+        return self._remote_codecs.get(name)
+
+    def get_remote_candidates(self, name):
+        return self._remote_candidates.get(name)
+
+    def set_local_codecs(self, name, codecs):
+        self._local_codecs[name] = codecs
+
+    def set_local_candidates(self, name, candidates):
+        self._local_candidates[name] = candidates
+
+    def set_active_candidates(self, name, local, remote):
+        self._local_active[name] = local
+        self._remote_active[name] = remote
+
+    def build_sdp(self):
+        sdp = Message()
+        for name, codecs in self._local_codecs.iteritems():
+            media = self.build_media(name, codecs)
+            sdp.medias[media.name] = media
+        return sdp.build()
+
+    def build_media(self, name, codecs):
+        ip, port, rtcp = self.get_default_address(name)
+        media = Media(name, ip, port, rtcp)
+        media.codecs = codecs
+
+        candidates = self.get_active_local_candidates(name)
+        if candidates:
+            if self.draft is 19:
+                media.add_attribute("ice-ufrag", candidates[0].username)
+                media.add_attribute("ice-pwd", candidates[0].password)
+            for candidate in candidates:
+                media.add_attribute("candidate", candidate.build_local())
+
+        candidates = self.get_active_remote_candidates(name)
+        if candidates:
+            list = map(lambda c: c.build_remote(), candidates)
+            media.add_attribute("remote-candidate", " ".join(list))
+
+        return media
+
+    def parse_sdp(self, message):
+        sdp = Message()
+        sdp.parse(message)
+        for media in sdp.medias.values():
+            self.parse_media(media)
+
+    def parse_media(self, media):
+        self._remote_codecs[media.name] = media.codecs
+        self._remote_candidates[media.name] = self.parse_candidates(media)
+
+    def parse_candidates(self, media):
+        candidates = []
+
+        if not media.get_attribute("candidate"):
+            self.draft = 0
+            candidates.append(Candidate(component_id=COMPONENTS.RTP, ip=media.ip,
+                port=media.port, transport="UDP", priority=1, type="host"))
+            candidates.append(Candidate(component_id=COMPONENTS.RTCP, ip=media.ip,
+                port=media.rtcp, transport="UDP", priority=1, type="host"))
+        else:
+            ufrag = media.get_attribute("ice-ufrag")
+            pwd = media.get_attribute("ice-pwd")
+            if ufrag and pwd:
+                self.draft = 19
+            else:
+                self.draft = 6
+
+            for attribute in media.get_attributes("candidate"):
+                candidate = Candidate(draft=self.draft, username=ufrag,
+                                      password=pwd)
+                candidate.parse(attribute)
+                candidates.append(candidate)
+
+        return candidates
+
+    def get_active_local_candidates(self, name):
+        active = self._local_active.get(name)
+        candidates = self._local_candidates.get(name)
+        if active:
+            return reduce(lambda x: (x.foundation == active.foundation), candidates)
+        return candidates
+
+    def get_active_remote_candidates(self, name):
+        candidates = []
+        components = []
+        active = self._remote_active.get(name)
+        if active is None:
+            return candidates
+        for candidate in self._remote_candidates.get(name):
+            if candidate.foundation == active.foundation:
+                if self.draft is 6:
+                    candidates.append(candidate)
+                    break
+                elif self.draft is 19:
+                    if candidate.component_id in components:
+                        continue
+                    candidates.append(candidate)
+                    components.append(candidate.component_id)
+        return candidates
+
+    def get_default_address(self, name):
+        ip = None
+        port = None
+        rtcp = None
+
+        active = self._local_active.get(name)
+        if not active:
+            active = self.search_relay(name)
+
+        for candidate in self._local_candidates.get(name):
+            if candidate.foundation == active.foundation and \
+               candidate.component_id is COMPONENTS.RTP:
+                ip = candidate.ip
+                port = candidate.port
+            if candidate.foundation == active.foundation and \
+               candidate.component_id is COMPONENTS.RTCP:
+                rtcp = candidate.port
+
+        return ip, port, rtcp
+
+    def search_relay(self, name):
+        relay = None
+        for candidate in self._local_candidates.get(name):
+            if candidate.transport != "UDP":
+                continue
+            if candidate.is_relay():
+                return candidate
+            if not relay or candidate.priority < relay.priority:
+                relay = candidate
+        return relay
 
 
 class Candidate(object):
@@ -68,6 +212,12 @@ class Candidate(object):
             self._extensions["rport"] = value
         return locals()
 
+    def is_relay(self):
+        if self.draft is 6:
+            return (self.priority < 0.5)
+        elif self.draft is 19:
+            return (self.type == "relay")
+
     def build_local(self):
         if self.draft is 6:
             return "%s %i %s %s %.3f %s %i" % (self.username, self.component_id,
@@ -100,3 +250,20 @@ class Candidate(object):
                 self.priority, self.ip, self.port) = parts[0:7]
             self.foundation = self.username[0:31]
             self.priority = float(self.priority)
+
+    def __eq__(self, other):
+        return (self.draft == other.draft and
+                self.foundation == other.foundation and
+                self.component_id == other.component_id and
+                self.transport == other.transport and
+                self.priority == other.priority and
+                self.username == other.username and
+                self.password == other.password and
+                self.type == other.type and
+                self.ip == other.ip and
+                self.port == other.port and
+                self.base_ip == other.base_ip and
+                self.base_port == other.base_port)
+
+    def __repr__(self):
+        return "<ICE Candidate: %s>" % self.build_local()
--- tests/test_ice.py
+++ tests/test_ice.py
@@ -21,7 +21,7 @@
 import sys
 import unittest
 
-definitions19 = [
+audio_candidates19 = [
 ((1, 1, "UDP", 2013266431, "192.168.1.107", 54751, "host", None, None),
 "1 1 UDP 2013266431 192.168.1.107 54751 typ host"),
 ((3, 1, "UDP", 1677721855, "70.25.46.249", 54751, "srflx", "192.168.1.107", 54751),
@@ -35,7 +35,17 @@ definitions19 = [
 ((4, 2, "UDP", 1006633214, "64.4.35.48", 45694, "relay", "192.168.1.107", 49259),
 "4 2 UDP 1006633214 64.4.35.48 45694 typ relay raddr 192.168.1.107 rport 49259")]
 
-definitions6 = [
+video_candidates19 = [
+((2, 1, "UDP", 2013266431, "192.168.1.116", 52826, "host", None, None),
+"2 1 UDP 2013266431 192.168.1.116 52826 typ host"),
+((5, 1, "UDP", 1677721855, "69.70.191.106", 52826, "srflx", "192.168.1.116", 52826),
+"5 1 UDP 1677721855 69.70.191.106 52826 typ srflx raddr 192.168.1.116 rport 52826"),
+((2, 2, "UDP", 2013266430, "192.168.1.116", 37649, "host", None, None),
+"2 2 UDP 2013266430 192.168.1.116 37649 typ host"),
+((5, 2, "UDP", 1677721854, "69.70.191.106", 37649, "srflx", "192.168.1.116", 37649),
+"5 2 UDP 1677721854 69.70.191.106 37649 typ srflx raddr 192.168.1.116 rport 37649")]
+
+audio_candidates6 = [
 (("gAH6Rj7UAhyhL37x1myyRCEe0s90i/okTPPlQ8q9Ufg=", 1,
   "AT6jQtrWTfVi7S1Ko4pBxA==", "UDP", 0.830, "192.168.1.107", 54183),
 "gAH6Rj7UAhyhL37x1myyRCEe0s90i/okTPPlQ8q9Ufg= 1 AT6jQtrWTfVi7S1Ko4pBxA== UDP 0.830 192.168.1.107 54183"),
@@ -55,6 +65,70 @@ definitions6 = [
   "iVxzBYjmxFxOHI4e3ZMq6A==", "UDP", 0.450, "64.4.34.204", 56585),
 "AHx6YkMxxV4F4diHF2o/wi6PF3hK8UPg/veO1nkC8CY= 2 iVxzBYjmxFxOHI4e3ZMq6A== UDP 0.450 64.4.34.204 56585")]
 
+audio_codecs = [((8, "PCMA", 8000, None), "8 PCMA/8000", ""),
+                ((0, "PCMU", 8000, None), "0 PCMU/8000", ""),
+                ((101, "telephone-event", 8000, "0-16"), 
+                 "101 telephone-event/8000", "101 0-16")]
+
+video_codecs = [((34, "H263", 90000, None), "34 H263/90000", "")]
+
+audio_msg19 = """v=0
+o=- 0 0 IN IP4 64.4.34.205
+s=session
+b=CT:99980
+t=0 0
+m=audio 42821 RTP/AVP 8 0 101
+c=IN IP4 64.4.34.205
+a=ice-ufrag:kZ+J
+a=ice-pwd:LFlH+neJ0CmUG/Vm1inOCJ
+a=candidate:1 1 UDP 2013266431 192.168.1.107 54751 typ host
+a=candidate:3 1 UDP 1677721855 70.25.46.249 54751 typ srflx raddr 192.168.1.107 rport 54751
+a=candidate:4 1 UDP 1006633215 64.4.35.48 30797 typ relay raddr 192.168.1.107 rport 54751
+a=candidate:1 2 UDP 2013266430 192.168.1.107 49259 typ host
+a=candidate:3 2 UDP 1677721854 70.25.46.249 49259 typ srflx raddr 192.168.1.107 rport 49259
+a=candidate:4 2 UDP 1006633214 64.4.35.48 45694 typ relay raddr 192.168.1.107 rport 49259
+a=rtcp:41965
+a=rtpmap:8 PCMA/8000
+a=rtpmap:0 PCMU/8000
+a=rtpmap:101 telephone-event/8000
+a=fmtp:101 0-16
+a=encryption:rejected"""
+
+video_msg19 = """m=video 52826 RTP/AVP 34
+c=IN IP4 192.168.1.116
+a=x-caps:34 65537:352:288:15.0:256000:1;131074:176:144:15.0:180000:1
+a=ice-ufrag:JjzZ
+a=ice-pwd:lfQttyfZ3SicOvs1WC2del
+a=candidate:2 1 UDP 2013266431 192.168.1.116 52826 typ host
+a=candidate:5 1 UDP 1677721855 69.70.191.106 52826 typ srflx raddr 192.168.1.116 rport 52826
+a=candidate:2 2 UDP 2013266430 192.168.1.116 37649 typ host
+a=candidate:5 2 UDP 1677721854 69.70.191.106 37649 typ srflx raddr 192.168.1.116 rport 37649
+a=rtcp:37649
+a=rtpmap:34 H263/90000
+a=encryption:rejected"""
+
+
+def createCandidate19(args):
+    return Candidate(draft=19, 
+                     foundation=args[0],
+                     component_id=args[1],
+                     transport=args[2],
+                     priority=args[3],
+                     ip=args[4],
+                     port=args[5],
+                     type=args[6],
+                     base_ip=args[7],
+                     base_port=args[8])
+
+def createCandidate6(args):
+    return Candidate(draft=6, 
+                     username=args[0],
+                     component_id=args[1],
+                     password=args[2],
+                     transport=args[3],
+                     priority=args[4],
+                     ip=args[5],
+                     port=args[6])
 
 class CandidateTestCase(unittest.TestCase):
 
@@ -81,65 +155,124 @@ class CandidateTestCase(unittest.TestCase):
         self.assertEqual(candidate.ip, args[5])
         self.assertEqual(candidate.port, args[6])
 
-    def createCandidate19(self, args):
-        return Candidate(draft=19, 
-                         foundation=args[0],
-                         component_id=args[1],
-                         transport=args[2],
-                         priority=args[3],
-                         ip=args[4],
-                         port=args[5],
-                         type=args[6],
-                         base_ip=args[7],
-                         base_port=args[8])
-
-    def createCandidate6(self, args):
-        return Candidate(draft=6, 
-                         username=args[0],
-                         component_id=args[1],
-                         password=args[2],
-                         transport=args[3],
-                         priority=args[4],
-                         ip=args[5],
-                         port=args[6])
-
     def testParse19(self):
-        for args, line in definitions19:
+        for args, line in audio_candidates19:
             candidate = Candidate(draft=19)
             candidate.parse(line)
             self.assertCandidate19(candidate, args)
 
     def testBuildLocal19(self):
-        for args, line in definitions19:
-            candidate = self.createCandidate19(args)
+        for args, line in audio_candidates19:
+            candidate = createCandidate19(args)
             print candidate.build_local()
 
     def testBuildParse19(self):
-        for args, line in definitions19:
-            candidate = self.createCandidate19(args)
+        for args, line in audio_candidates19:
+            candidate = createCandidate19(args)
             line = candidate.build_local()
             candidate = Candidate(draft=19)
             candidate.parse(line)
             self.assertCandidate19(candidate, args)
 
     def testParse6(self):
-        for args, line in definitions6:
+        for args, line in audio_candidates6:
             candidate = Candidate(draft=6)
             candidate.parse(line)
             self.assertCandidate6(candidate, args)
 
     def testBuildLocal6(self):
-        for args, line in definitions6:
-            candidate = self.createCandidate6(args)
+        for args, line in audio_candidates6:
+            candidate = createCandidate6(args)
             self.assertEqual(candidate.build_local(), line)
 
     def testBuildParse19(self):
-        for args, line in definitions6:
-            candidate = self.createCandidate6(args)
+        for args, line in audio_candidates6:
+            candidate = createCandidate6(args)
             line = candidate.build_local()
             candidate = Candidate(draft=6)
             candidate.parse(line)
             self.assertCandidate6(candidate, args)
+
+
+class Session19TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.session = Session(draft=19)
+        self.audio_candidates = []
+        for args, line in audio_candidates19:
+            candidate = createCandidate19(args)
+            candidate.username = "kZ+J"
+            candidate.password = "LFlH+neJ0CmUG/Vm1inOCJ"
+            self.audio_candidates.append(candidate)
+        self.video_candidates = []
+        for args, line in video_candidates19:
+            candidate = createCandidate19(args)
+            candidate.username = "JjzZ"
+            candidate.password = "lfQttyfZ3SicOvs1WC2del"
+            self.video_candidates.append(candidate)
+
+        self.audio_codecs = []
+        for args, rtpmap, fmtp in audio_codecs:
+            self.audio_codecs.append(Codec(*args))
+        self.video_codecs = []
+        for args, rtpmap, fmtp in video_codecs:
+            self.video_codecs.append(Codec(*args))
+
+    def testGetAudioRelay(self):
+        self.session.set_local_candidates("audio", self.audio_candidates)
+        relay = self.session.search_relay("audio")
+        self.assertEqual(relay.foundation, 4)
+
+    def testGetVideoRelay(self):
+        self.session.set_local_candidates("video", self.video_candidates)
+        relay = self.session.search_relay("video")
+        self.assertEqual(relay.foundation, 5)
+
+    def testParseSdpA(self):
+        self.session.parse_sdp(audio_msg19)
+        self.assertEqual(self.session.get_remote_codecs("audio"),
+                         self.audio_codecs)
+        self.assertEqual(self.session.get_remote_candidates("audio"),
+                         self.audio_candidates)
+
+    def testParseSdpAV(self):
+        self.session.parse_sdp(audio_msg19 + "\r\n" + video_msg19)
+        self.assertEqual(self.session.get_remote_codecs("audio"),
+                         self.audio_codecs)
+        self.assertEqual(self.session.get_remote_candidates("audio"),
+                         self.audio_candidates)
+        self.assertEqual(self.session.get_remote_codecs("video"),
+                         self.video_codecs)
+        self.assertEqual(self.session.get_remote_candidates("video"),
+                         self.video_candidates)
+
+    def testBuildParseSdpA(self):
+        self.session.set_local_codecs("audio", self.audio_codecs)
+        self.session.set_local_candidates("audio", self.audio_candidates)
+        msg = self.session.build_sdp()
+        self.session.parse_sdp(msg)
+        self.assertEqual(self.session.get_remote_codecs("audio"),
+                         self.audio_codecs)
+        self.assertEqual(self.session.get_remote_candidates("audio"),
+                         self.audio_candidates)
+        self.assertEqual(self.session.get_remote_codecs("video"), None)
+        self.assertEqual(self.session.get_remote_candidates("video"), None)
+
+    def testBuildParseSdpAV(self):
+        self.session.set_local_codecs("audio", self.audio_codecs)
+        self.session.set_local_candidates("audio", self.audio_candidates)
+        self.session.set_local_codecs("video", self.video_codecs)
+        self.session.set_local_candidates("video", self.video_candidates)
+        msg = self.session.build_sdp()
+        self.session.parse_sdp(msg)
+        self.assertEqual(self.session.get_remote_codecs("audio"),
+                         self.audio_codecs)
+        self.assertEqual(self.session.get_remote_candidates("audio"),
+                         self.audio_candidates)
+        self.assertEqual(self.session.get_remote_codecs("video"),
+                         self.video_codecs)
+        self.assertEqual(self.session.get_remote_candidates("video"),
+                         self.video_candidates)
 
 
 if __name__ == "__main__":
