2d803ac4979c0237eecf67d50e91247ec09d1ffd
Louis-Francis Ratté-Boulianne <louis-francis.ratte-boulianne@collabora.co.uk>
2009-05-15 12:19:29 -0400

    Added ICE candidates parsing and building (and tests)

--- papyon/sip/ice.py
+++ papyon/sip/ice.py
@@ -18,8 +18,85 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+from papyon.util.decorator import rw_property
+
 class Session(object):
     pass
 
+
 class Candidate(object):
-    pass
+
+    def __init__(self, draft=0, foundation=None, component_id=None,
+                 transport=None, priority=None, username=None, password=None,
+                 type=None, ip=None, port=None, base_ip=None, base_port=None):
+        self._extensions = {}
+
+        self.draft = draft
+        self.foundation = foundation
+        self.component_id = component_id
+        self.transport = transport
+        self.priority = priority
+        self.username = username
+        self.password = password
+        self.type = type
+        self.ip = ip
+        self.port = port
+        self.base_ip = base_ip
+        self.base_port = base_port
+
+    @rw_property
+    def type():
+        def fget(self):
+            return self._extensions.get("typ")
+        def fset(self, value):
+            self._extensions["typ"] = value
+        return locals()
+
+    @rw_property
+    def base_ip():
+        def fget(self):
+            return self._extensions.get("raddr")
+        def fset(self, value):
+            self._extensions["raddr"] = value
+        return locals()
+
+    @rw_property
+    def base_port():
+        def fget(self):
+            return self._extensions.get("rport")
+        def fset(self, value):
+            self._extensions["rport"] = value
+        return locals()
+
+    def build_local(self):
+        if self.draft is 6:
+            return "%s %i %s %s %.3f %s %i" % (self.username, self.component_id,
+                self.password, self.transport, self.priority, self.ip, self.port)
+        elif self.draft is 19:
+            ext = []
+            for k, v in self._extensions.iteritems():
+                if v is not None:
+                    ext.append("%s %s" % (k, str(v)))
+            return "%i %i %s %i %s %i %s" % (self.foundation, self.component_id,
+                self.transport, self.priority, self.ip, self.port, " ".join(ext))
+
+    def build_remote(self):
+        if self.draft is 6:
+            return self.username
+        elif self.draft is 19:
+            return "%i %s %i" % (self.component_id, self.ip, self.port)
+
+    def parse(self, line):
+        parts = line.split()
+        parts = map(lambda x: ((x.isdigit() and int(x)) or x), parts)
+
+        if self.draft is 19:
+            (self.foundation, self.component_id, self.transport,
+                self.priority, self.ip, self.port) = parts[0:6]
+            for i in range(6, len(parts), 2):
+                self._extensions[parts[i]] = parts[i + 1]
+        elif self.draft is 6:
+            (self.username, self.component_id, self.password, self.transport,
+                self.priority, self.ip, self.port) = parts[0:7]
+            self.foundation = self.username[0:31]
+            self.priority = float(self.priority)
--- tests/test_ice.py
+++ tests/test_ice.py
@@ -17,3 +17,132 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import sys
+import unittest
+
+definitions19 = [
+((1, 1, "UDP", 2013266431, "192.168.1.107", 54751, "host", None, None),
+"1 1 UDP 2013266431 192.168.1.107 54751 typ host"),
+((3, 1, "UDP", 1677721855, "70.25.46.249", 54751, "srflx", "192.168.1.107", 54751),
+"3 1 UDP 1677721855 70.25.46.249 54751 typ srflx raddr 192.168.1.107 rport 54751"),
+((4, 1, "UDP", 1006633215, "64.4.35.48", 30797, "relay", "192.168.1.107", 54751),
+"4 1 UDP 1006633215 64.4.35.48 30797 typ relay raddr 192.168.1.107 rport 54751"),
+((1, 2, "UDP", 2013266430, "192.168.1.107", 49259, "host", None, None),
+"1 2 UDP 2013266430 192.168.1.107 49259 typ host"),
+((3, 2, "UDP", 1677721854, "70.25.46.249", 49259, "srflx", "192.168.1.107", 49259),
+"3 2 UDP 1677721854 70.25.46.249 49259 typ srflx raddr 192.168.1.107 rport 49259"),
+((4, 2, "UDP", 1006633214, "64.4.35.48", 45694, "relay", "192.168.1.107", 49259),
+"4 2 UDP 1006633214 64.4.35.48 45694 typ relay raddr 192.168.1.107 rport 49259")]
+
+definitions6 = [
+(("gAH6Rj7UAhyhL37x1myyRCEe0s90i/okTPPlQ8q9Ufg=", 1,
+  "AT6jQtrWTfVi7S1Ko4pBxA==", "UDP", 0.830, "192.168.1.107", 54183),
+"gAH6Rj7UAhyhL37x1myyRCEe0s90i/okTPPlQ8q9Ufg= 1 AT6jQtrWTfVi7S1Ko4pBxA== UDP 0.830 192.168.1.107 54183"),
+(("wknsOPNTNLvDhc7jX+qIKK/1YwcG+8uifjfo21ridEM=", 1,
+  "wWRgVlW33BSPYwEZrsFwFg==", "UDP", 0.550, "70.25.46.249", 54183),
+"wknsOPNTNLvDhc7jX+qIKK/1YwcG+8uifjfo21ridEM= 1 wWRgVlW33BSPYwEZrsFwFg== UDP 0.550 70.25.46.249 54183"),
+(("AHx6YkMxxV4F4diHF2o/wi6PF3hK8UPg/veO1nkC8CY=", 1,
+  "iVxzBYjmxFxOHI4e3ZMq6A==", "UDP", 0.450, "64.4.34.204", 32594),
+"AHx6YkMxxV4F4diHF2o/wi6PF3hK8UPg/veO1nkC8CY= 1 iVxzBYjmxFxOHI4e3ZMq6A== UDP 0.450 64.4.34.204 32594"),
+(("gAH6Rj7UAhyhL37x1myyRCEe0s90i/okTPPlQ8q9Ufg=", 2,
+  "AT6jQtrWTfVi7S1Ko4pBxA==", "UDP", 0.830, "192.168.1.107", 43701),
+"gAH6Rj7UAhyhL37x1myyRCEe0s90i/okTPPlQ8q9Ufg= 2 AT6jQtrWTfVi7S1Ko4pBxA== UDP 0.830 192.168.1.107 43701"),
+(("wknsOPNTNLvDhc7jX+qIKK/1YwcG+8uifjfo21ridEM=", 2,
+  "wWRgVlW33BSPYwEZrsFwFg==", "UDP", 0.550, "70.25.46.249", 43701),
+"wknsOPNTNLvDhc7jX+qIKK/1YwcG+8uifjfo21ridEM= 2 wWRgVlW33BSPYwEZrsFwFg== UDP 0.550 70.25.46.249 43701"),
+(("AHx6YkMxxV4F4diHF2o/wi6PF3hK8UPg/veO1nkC8CY=", 2,
+  "iVxzBYjmxFxOHI4e3ZMq6A==", "UDP", 0.450, "64.4.34.204", 56585),
+"AHx6YkMxxV4F4diHF2o/wi6PF3hK8UPg/veO1nkC8CY= 2 iVxzBYjmxFxOHI4e3ZMq6A== UDP 0.450 64.4.34.204 56585")]
+
+
+class CandidateTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def assertCandidate19(self, candidate, args):
+        self.assertEqual(candidate.foundation, args[0])
+        self.assertEqual(candidate.component_id, args[1])
+        self.assertEqual(candidate.transport, args[2])
+        self.assertEqual(candidate.priority, args[3])
+        self.assertEqual(candidate.ip, args[4])
+        self.assertEqual(candidate.port, args[5])
+        self.assertEqual(candidate.type, args[6])
+        self.assertEqual(candidate.base_ip, args[7])
+        self.assertEqual(candidate.base_port, args[8])
+
+    def assertCandidate6(self, candidate, args):
+        self.assertEqual(candidate.username, args[0])
+        self.assertEqual(candidate.component_id, args[1])
+        self.assertEqual(candidate.password, args[2])
+        self.assertEqual(candidate.transport, args[3])
+        self.assertEqual(candidate.priority, args[4])
+        self.assertEqual(candidate.ip, args[5])
+        self.assertEqual(candidate.port, args[6])
+
+    def createCandidate19(self, args):
+        return Candidate(draft=19, 
+                         foundation=args[0],
+                         component_id=args[1],
+                         transport=args[2],
+                         priority=args[3],
+                         ip=args[4],
+                         port=args[5],
+                         type=args[6],
+                         base_ip=args[7],
+                         base_port=args[8])
+
+    def createCandidate6(self, args):
+        return Candidate(draft=6, 
+                         username=args[0],
+                         component_id=args[1],
+                         password=args[2],
+                         transport=args[3],
+                         priority=args[4],
+                         ip=args[5],
+                         port=args[6])
+
+    def testParse19(self):
+        for args, line in definitions19:
+            candidate = Candidate(draft=19)
+            candidate.parse(line)
+            self.assertCandidate19(candidate, args)
+
+    def testBuildLocal19(self):
+        for args, line in definitions19:
+            candidate = self.createCandidate19(args)
+            print candidate.build_local()
+
+    def testBuildParse19(self):
+        for args, line in definitions19:
+            candidate = self.createCandidate19(args)
+            line = candidate.build_local()
+            candidate = Candidate(draft=19)
+            candidate.parse(line)
+            self.assertCandidate19(candidate, args)
+
+    def testParse6(self):
+        for args, line in definitions6:
+            candidate = Candidate(draft=6)
+            candidate.parse(line)
+            self.assertCandidate6(candidate, args)
+
+    def testBuildLocal6(self):
+        for args, line in definitions6:
+            candidate = self.createCandidate6(args)
+            self.assertEqual(candidate.build_local(), line)
+
+    def testBuildParse19(self):
+        for args, line in definitions6:
+            candidate = self.createCandidate6(args)
+            line = candidate.build_local()
+            candidate = Candidate(draft=6)
+            candidate.parse(line)
+            self.assertCandidate6(candidate, args)
+
+
+if __name__ == "__main__":
+    sys.path.insert(0, "")
+    from papyon.sip.ice import *
+    unittest.main()
