63767ffe270db179bd85b4020b551f03bab88815
Louis-Francis Ratté-Boulianne <louis-francis.ratte-boulianne@collabora.co.uk>
2009-05-20 11:53:34 -0400

    Added main frame to send and receive messages

--- papyon/sip/sip.py
+++ papyon/sip/sip.py
@@ -22,11 +22,166 @@ from papyon.sip.constants import *
 
 import gobject
 
-class Connection(object):
-    pass
+class Connection(gobject.GObject):
 
-class Call(object):
-    pass
+    def __init__(self, transport, user, password):
+        gobject.GObject.__init__(self)
+        self._calls = {}
+        self._parser = MessageParser()
+        self._parser.connect("message-received", self.on_message_received)
+        self._transport = transport
+        self._transport.connect("line-received", self._parser.on_line_received)
+
+    def on_message_received(self, parser, message):
+        if not call:
+            if isinstance(message, Request) and message.code == "INVITE":
+                callid = message.get_header("Call-ID")
+                call = Call(self._transport, callid)
+                self._calls[callid] = call
+            else:
+                pass #something's wrong
+        call.on_message_received(message)
+
+
+class BaseCall(object):
+
+    def __init__(self, transport, callid=None, tunneled=False):
+        self._transport = transport
+        self._callid = callid
+        self._tunneled = tunneled
+        self._cseq = random.randint(1000, 5000)
+
+    def gen_call_id(self):
+        return 400000000 + random.randint(0,2000000)
+
+    def gen_hex(self):
+        ret  = ('%04x'%(random.randint(0, 2**10)))[:4]
+        ret += ('%04x'%(random.randint(0, 2**10)))[:4]
+        return ret
+
+    def gen_mepid(self):
+        pass
+
+    def get_call_id(self):
+        if not self._callid:
+            self._callid = self.gen_call_id()
+        return self._callid
+
+    def get_cseq(self, incr=False):
+        if incr:
+            self._cseq += 1
+        return self._cseq
+
+    def get_epid(self):
+        if not hasattr(self, '_epid'):
+            self._epid = self.gen_hex()
+        return self._epid
+
+    def get_mepid(self):
+        if not self._tunneled:
+            return ""
+        if not hasattr(self, '_mepid'):
+            self._mepid = self.gen_mepid()
+        return ";mepid=%s" % self._mepid
+
+    def get_tag(self):
+        if not hasattr(self, '_tag'):
+            self._tag = self.gen_hex()
+        return self._tag
+
+    def get_local_address(self):
+        return (self._transport.ip, self._transport.port)
+
+    def send(self, message):
+        self._transport.send(str(message))
+
+    def build_request(self, code, uri=None, to=None, name="", user="", incr=False):
+        request = Request(code, uri)
+        request.add_header("Via", "SIP/2.0/%s %s:%s" %
+            (self._transport.protocol, self.get_local_address())
+        request.add_header("Max-Forwards", 70)
+        request.add_header("Call-ID", self.get_call_id())
+        request.add_header("CSeq", "%i %s" % (self.get_cseq(incr), code))
+        request.add_header("To", to)
+        request.add_header("From", "%s<sip:%s%s>;tag=%s;epid=%s" %
+            (name, user, self.get_mepid(), self.get_tag(), self.get_epid()))
+        request.add_header("User-Agent", USER_AGENT)
+        return request
+
+    def build_response(self, request, status, reason=None):
+        response = Response(status, reason)
+        for via in request.get_headers("Via"):
+            response.add_header("Via", via)
+        response.add_header("Max-Forwards", 70)
+        response.add_header("From", request.get_header("From"))
+        response.add_header("To", request.get_header("To"))
+        response.add_header("Call-ID", self.get_call_id())
+        response.add_header("CSeq", request.get_header("CSeq"))
+        response.add_header("User-Agent", self.USER_AGENT)
+        return response
+
+    def on_message_received(self, msg):
+        if type(msg) is Response:
+            handler_name = "on_%s_response" % msg.code.lower()
+        elif type(msg) is Request:
+            handler_name = "on_%s_received" % msg.code.lower()
+        handler = getattr(self, handler_name, None)
+        if handler is not None:
+            handler(msg)
+
+
+class Call(BaseCall):
+
+    def invite(self, uri):
+        pass
+
+    def accept(self):
+        pass
+
+    def reject(self):
+        pass
+
+    def send_ack(self):
+        pass
+
+    def cancel(self):
+        pass
+
+    def send_bye(self):
+        pass
+
+    def on_invite_received(self, invite):
+        pass
+
+    def on_ack_received(self, ack):
+        pass
+
+    def on_cancel_received(self, cancel):
+        pass
+
+    def on_bye_received(self, bye):
+        pass
+
+    def on_invite_response(self, response):
+        pass
+
+    def on_cancel_response(self, response):
+        pass
+
+    def on_bye_response(self, response):
+        pass
+
+
+class Registration(BaseCall):
+
+    def register(self):
+        pass
+
+    def unregister(self):
+        pass
+
+    def on_register_response(self, response):
+        pass
 
 
 class Message(object):
