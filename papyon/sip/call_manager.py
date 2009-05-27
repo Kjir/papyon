71e5b8ee2a60d236881532a88de555088f5be5fc
Louis-Francis Ratté-Boulianne <louis-francis.ratte-boulianne@collabora.co.uk>
2009-05-27 14:42:11 -0400

    Added a SIP call manager class to manage all connections

---
+++ papyon/sip/call_manager.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+#
+# papyon - a python client library for Msn
+#
+# Copyright (C) 2009 Collabora Ltd.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+from papyon.sip.sip import *
+from papyon.sip.transport import *
+
+import gobject
+
+class SIPCallManager(gobject.GObject):
+
+    __gsignals__ = {
+        'invite-received': (gobject.SIGNAL_RUN_FIRST,
+            gobject.TYPE_NONE,
+            ([object]))
+    }
+
+    server = "vp.sip.messenger.msn.com"
+    port = 443
+
+    def __init__(self, client):
+        gobject.GObject.__init__(self)
+        self._client = client
+        self._connections = {}
+
+    def create_connection(self, tunneled, host=None):
+        if not tunneled:
+            transport = SIPTransport(host, self.port)
+            account = self._client.profile.account
+            password = str(self._client.profile.password)
+            sso = self._client._sso
+            connection = SIPConnection(transport, sso, account, password)
+        connection.connect("invite-received", self.on_invite_received)
+        self._connections[host] = connection
+        return connection
+
+    def get_connection(self, tunneled, host=None):
+        connection = self._connections.get(host, None)
+        if connection is None:
+            connection = self.create_connection(tunneled, host)
+        return connection
+
+    def invite(self, uri, tunneled=False):
+        connection = self.get_connection(tunneled, self.server)
+        account = self._client.profile.account
+        call = connection.create_call(account, tunneled)
+        call.invite(uri)
+        return call
+
+    def on_notification_received(self, notification):
+        args = notification.payload.split()
+        if len(args) == 3 and args[0] == "INVITE":
+            # Register to the server so we can take the call
+            connection = self.get_connection(False, args[1])
+            connection.register(None, None)
+
+    def on_invite_received(self, connection, call):
+        self.emit("invite-received", call)
