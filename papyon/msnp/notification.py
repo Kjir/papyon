87e9bc961afebf96835bf12366f25f1532039836
Louis-Francis Ratté-Boulianne <louis-francis.ratte-boulianne@collabora.co.uk>
2009-07-28 14:06:59 -0400

    Don't automatically block deleted contacts
    We only need to remove them from the FORWARD list, nothing else.

--- papyon/msnp/notification.py
+++ papyon/msnp/notification.py
@@ -702,12 +702,6 @@ class NotificationProtocol(BaseProtocol, gobject.GObject):
 
     def _address_book_contact_deleted_cb(self, address_book, contact):
         self.remove_contact_from_membership(contact.account, contact.network_id,
-                                            profile.Membership.ALLOW)
-
-        self.add_contact_to_membership(contact.account, contact.network_id,
-                                       profile.Membership.BLOCK)
-
-        self.remove_contact_from_membership(contact.account, contact.network_id,
                                             profile.Membership.FORWARD)
 
     def _address_book_contact_accepted_cb(self, address_book, contact):
