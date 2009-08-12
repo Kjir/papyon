d300b73179eb3e1bb1d69ad601a970542c75e3b4
Louis-Francis Ratté-Boulianne <louis-francis.ratte-boulianne@collabora.co.uk>
2009-08-12 13:54:15 -0400

    Keep the unicode string instead of encoding it to utf-8 (AB group name)

--- papyon/service/AddressBook/address_book.py
+++ papyon/service/AddressBook/address_book.py
@@ -232,7 +232,7 @@ class AddressBook(gobject.GObject):
             contacts = address_book.contacts
             groups = address_book.groups
             for group in groups:
-                g = profile.Group(group.Id, group.Name.encode("utf-8"))
+                g = profile.Group(group.Id, group.Name)
                 self.groups.add(g)
             for contact in contacts:
                 c = self.__build_contact(contact, Membership.FORWARD)
